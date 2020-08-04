c3a6de68c2b0eb23caf389e98d6009abe215d870
abraham <abraham@android-c546e1957a2ba564.home.sercomm>
2020-08-03 22:28:09 -0500

    update 1

--- CDN/01.js
+++ CDN/01.js
@@ -2,7 +2,8 @@ const app = new Vue(
     {
         el:  '#app',
         data:{
-           titulo: 'hola mundo con vue'
+           titulo: 'hola mundo con vue',
+           frutas: ['Manzana', 'Pera','Platano']
         }
 
     })
