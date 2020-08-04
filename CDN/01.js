1e6b0e0f5a5b6df1a1b3722d216d4dce0b844bc6
abraham <fpatan@mail.ru>
2020-08-04 08:50:24 -0500

    4

--- CDN/01.js
+++ CDN/01.js
@@ -2,8 +2,10 @@ const app = new Vue(
     {
         el:  '#app',
         data:{
+            
            titulo: 'hola mundo con vue',
            frutas: [
+
                 {nombre: 'Pera', cantidad: 0},
                 {nombre: 'Manzana', cantidad: 10},
                 {nombre: 'Naranja', cantidad: 20}
