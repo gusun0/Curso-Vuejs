2e110796d360a97e436acbfa1ae87ce0c190f210
abraham <fpatan@mail.ru>
2020-08-05 12:39:48 -0500

    CRUD con vanilla JS

--- JS/CRUD vainilla/app.js
+++ JS/CRUD vainilla/app.js
@@ -31,27 +31,43 @@ const PintarDB = () =>{
         arrayActividades = [];
     }else{
         arrayActividades.forEach(element => {
-            
-            listaActividadesUI.innerHTML += `        
-            <div class="alert alert-danger" role="alert">
-            <span class="material-icons float-left mr-2">
-                accessibility
-            </span> 
-                
-            <b>${element.actividad}</b> - ${element.estado}
-            <span class="float-right">
-                <span class="material-icons">
-                    done
-                </span>
-                <span class="material-icons">
-                    delete
-                    </span>
 
-            </span>                 
-          </div>`;
-        });
+            if(element.estado){
+                listaActividadesUI.innerHTML += `<div class="alert alert-success" role="alert"><span class="material-icons float-left mr-2">
+                accessibility</span><b>${element.actividad}</b> - ${element.estado}<span class="float-right"><span class="material-icons">done</span><span class="material-icons">delete</span></span></div>`;
+     
+
+            }else{
+
+                listaActividadesUI.innerHTML += `<div class="alert alert-danger" role="alert"><span class="material-icons float-left mr-2">
+                    accessibility</span><b>${element.actividad}</b> - ${element.estado}<span class="float-right"><span class="material-icons">done</span><span class="material-icons">delete</span></span></div>`;
+            }
+    }
+    );
+            
     }
 
+}
+
+const EliminarDB = (actividad) => {
+    //console.log(actividad);
+    let indexArray;
+    arrayActividades.forEach((elemento, index) => {
+        if(elemento.actividad === actividad){
+            indexArray = index;
+        }
+    });
+    arrayActividades.splice(indexArray,1);
+    GuardarDB();
+}
+
+const EditarDB = (actividad) =>{
+    let indexArray = arrayActividades.findIndex((elemento) => {
+       return elemento.actividad === actividad        
+    });
+    console.log(arrayActividades[indexArray]);
+    arrayActividades[indexArray].estado = true;
+    GuardarDB();
 }
 
 //eventslistener
@@ -69,9 +85,26 @@ formularioUI.addEventListener('submit',(e)=>{
 // EL DOM 
 document.addEventListener('DOMContentLoaded', PintarDB);
 
-/*listaActividadesUI.addEventListener('click', (e) => {
+listaActividadesUI.addEventListener('click', (e) => {
     
     e.preventDefault();
-    console.log(e);
-});*/
+    
+    //console.log(e.target.innerHTML);
+    
+    if(e.target.innerHTML === 'done' || e.target.innerHTML == 'delete'){
+        //console.log('accion del done');
+        //  console.log(e);
+       // console.log(e.path[2].childNodes[1].innerHTML);
+       let texto =  e.path[2].childNodes[1].innerHTML;
+    if(e.target.innerHTML === 'delete'){
+        EliminarDB(texto);
+    }
+
+    if(e.target.innerHTML === 'done'){
+        //accion de editar
+        EditarDB(texto);
+    }
+    
+    }
+});
 
