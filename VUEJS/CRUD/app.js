e74e6d04a15ad85597a22d0027e07c2e350debef
abraham <fpatan@mail.ru>
2020-08-05 14:38:26 -0500

    complete CRUD

--- VUEJS/CRUD/app.js
+++ VUEJS/CRUD/app.js
@@ -1,6 +1,53 @@
 const app = new Vue({
   
-    el: // un objeto  
-
+    el: '#app', // un objeto  
+    data: {
     
-})
+        titulo: "GYM CON VUE",
+        tareas: [],
+        nuevaTarea: ''
+      },
+
+    methods:{
+        agregarTarea: function()
+        {
+            console.log('diste click ',this.nuevaTarea);
+            this.tareas.push(
+            {
+                nombre: this.nuevaTarea,
+                estado: false
+            });
+            console.log(this.tareas);
+            this.nuevaTarea = '';
+
+            localStorage.setItem('gym-vue',JSON.stringify(this.tareas)); 
+        },
+        
+        editarTarea: function(index)
+        {
+            this.tareas[index].estado = true;
+            localStorage.setItem('gym-vue',JSON.stringify(this.tareas)); 
+        },
+
+        eliminar: function(index){
+            console.log(index);
+            this.tareas.splice(index,1);
+            localStorage.setItem('gym-vue',JSON.stringify(this.tareas)); 
+
+        }
+
+    },
+
+    created: function(){
+        let datosDB = JSON.parse(localStorage.getItem('gym-vue'));
+        console.log(datosDB);
+        
+        if(datosDB === null){
+            this.tareas = [];        
+        }else{
+            this.tareas = datosDB;
+        }
+
+    }
+
+});
