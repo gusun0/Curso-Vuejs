c4c4f518435cc1a8a5f62a8ec81f311a8909f914
abraham <fpatan@mail.ru>
2020-08-06 13:44:14 -0500

    final

---
+++ JS/tuto/FETCH/Fetch2/app.js
@@ -0,0 +1,29 @@
+var contenido = document.querySelector('#contenido')
+
+function traer(){
+    fetch('tabla.json')
+    .then(res => res.json())
+    .then(data => {
+       // console.log(data);
+        tabla(data);
+    })
+
+}
+
+function tabla(data){
+    //console.log(data)
+    contenido.innerHTML = ''
+
+    for(let valor of data){
+        console.log(valor.nombre)
+
+        contenido.innerHTML += `
+        <tr>
+        <th scope="row">${valor.id}</th>
+        <td>${valor.nombre}</td>
+        <td>${valor.email}</td>
+        <td>${valor.estado ? 'Activo' : 'Eliminado'}</td>
+      </tr>
+        `
+    }
+}
