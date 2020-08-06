5d286b3a60008843df18c08be591350c4a1daccd
abraham <fpatan@mail.ru>
2020-08-06 00:19:00 -0500

    c

---
+++ JS/tuto/objetos/obj.js
@@ -0,0 +1,29 @@
+var arreglo = ['1',2,true];
+console.log(arreglo);
+
+var objeto = {
+    nombre: 'html',
+    duracion: 15,
+    estado: true,
+
+    capitulos:{
+        nombre: 'introduccion',
+        capitulos: 20
+    }
+}
+
+    console.log(objeto.capitulos.nombre);
+
+    var arrayObjetos = [
+        {
+            nombre: 'html',
+            estado: true
+        },
+        {
+            nombre: 'ht',
+            estado: true
+        }
+]
+
+    console.log(arrayObjetos[0].nombre);
+ 
