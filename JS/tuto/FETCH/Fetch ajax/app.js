fc424472ebf4dea996f8f4f929b002adfb77c4e2
abraham <fpatan@mail.ru>
2020-08-06 12:15:46 -0500

    fetch ajax

--- JS/tuto/FETCH/Fetch ajax/app.js
+++ JS/tuto/FETCH/Fetch ajax/app.js
@@ -4,14 +4,29 @@ var documento = document.querySelector('#contenido');
 
 function traer(){
 
-    fetch('texto.txt')
-    .then(data => data.text())
-
+    fetch('https://randomuser.me/api/')
+    .then(res => res.json())
+    
     .then(data => {
+        console.log(data.results[0]);
+        console.log(data.results[0].name);
+        documento.innerHTML = `
+        <img src="${data.results[0].picture.large}" width="100px" class="img-fluid rounded-circle">
+            <p>Nombre: ${data.results[0].name.first}</p>
+        
+        `;
 
-        console.log(data);
-        documento.innerHTML = `${data}`;
     })
+
+    
+    
+
+
+    // .then(data => {
+
+    //     console.log(data);
+    //     documento.innerHTML = `${data}`;
+    // })
 
     //documento.innerHTML = `ss`;
 }
