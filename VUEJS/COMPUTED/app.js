b9869173006a03e012f4d3b79cc73122ac501891
abraham <fpatan@mail.ru>
2020-08-07 20:26:06 -0500

    computed

--- VUEJS/COMPUTED/app.js
+++ VUEJS/COMPUTED/app.js
@@ -3,13 +3,24 @@
         el: '#app',
         data: {
             
+            contador: 0,
             mensaje: 'hola soy x'
         },
 
         // para trabajar con lógica se usa computed 
         computed:{
+            
             invertido(){
                 return this.mensaje.split('').reverse().join('');
+            },
+
+            color(){
+                return{
+                    // estamos retornando un objeto 
+                    'bg-success' : this.contador <= 10,
+                    'bg-warning' : this.contador > 10 && this.contador < 20,    
+                    'bg-danger' : this.contador >= 20
+                }
             }
         }
 
