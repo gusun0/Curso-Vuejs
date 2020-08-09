9c84e2f94fe3b8d6cdfcd9cf217ecc9637d3be0c
abraham <fpatan@mail.ru>
2020-08-08 20:09:30 -0500

    1

---
+++ VUEJS/Components communication/hijo.js
@@ -0,0 +1,14 @@
+Vue.component('hijo',{
+
+    template:
+    `
+    <div class=" py-5 bg-success text-center">
+    <h1>hola</h1>
+    </div>
+    `
+})
+
+// new Vue({
+//     el: '#app'
+// });
+
---
+++ VUEJS/Components communication/padre.js
@@ -0,0 +1,15 @@
+Vue.component('padre',{
+    template: //html
+    `
+    <div class="p-5 bg-dark text-white mt-3">
+        <h2>Componente PADRE</h2>  
+        <hijo></hijo>     
+    </div>
+
+    `
+})
+
+// new Vue({
+//     el: '#app'
+   
+// });
