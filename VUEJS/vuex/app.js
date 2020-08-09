c3b00924b911a121be3514bc3fa4cad32b5a2861
abraham <fpatan@mail.ru>
2020-08-08 22:22:41 -0500

    f

---
+++ VUEJS/vuex/app.js
@@ -0,0 +1,10 @@
+Vue.component('titulo',{
+
+    template: //html
+    `
+    <div>
+        <h1>numero: {{ $store.state.numero }}</h1>
+        <hijo></hijo>
+    </div>
+    `
+})
---
+++ VUEJS/vuex/hijo.js
@@ -0,0 +1,11 @@
+Vue.component('hijo',{
+    template:
+    `
+    <div>
+    <button @click="$store.commit('aumentar')">+</button>
+    <h1>numero en componente hijo: {{ $store.state.numero }}</h1>
+    </div>
+    `
+})
+
+// /* <button @click="$store.state.numero++">+</button> */
