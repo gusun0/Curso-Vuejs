52b8a9b6b5ce57785ae1f988b484cbaf1c44b123
abraham <fpatan@mail.ru>
2020-08-08 20:26:40 -0500

    f

--- VUEJS/Components communication/hijo.js
+++ VUEJS/Components communication/hijo.js
@@ -4,8 +4,11 @@ Vue.component('hijo',{
     `
     <div class=" py-5 bg-success text-center">
     <h1>hola</h1>
+    <h3>{{numero}}</h3>
     </div>
-    `
+    `,
+    // recibimos informacion 
+    props: ['numero']
 })
 
 // new Vue({
--- VUEJS/Components communication/padre.js
+++ VUEJS/Components communication/padre.js
@@ -1,12 +1,17 @@
 Vue.component('padre',{
-    template: //html
+    template: //html    //con los dos puntos hacemos un v-bind
     `
-    <div class="p-5 bg-dark text-white mt-3">
-        <h2>Componente PADRE</h2>  
-        <hijo></hijo>     
+    <div class="p-5 bg-dark text-white ">
+        <h2>Componente PADRE: {{numeroPadre}}</h2>     
+        <hijo :numero="numeroPadre"></hijo> 
     </div>
 
-    `
+    `,
+    data(){
+        return{
+            numeroPadre: 0
+        }
+    }
 })
 
 // new Vue({
