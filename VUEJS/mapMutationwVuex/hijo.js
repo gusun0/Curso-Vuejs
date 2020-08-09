2334f3ccd20c0cbb25220336a1056825b3dfd930
abraham <fpatan@mail.ru>
2020-08-09 00:51:52 -0500

    final

--- VUEJS/mapMutationwVuex/hijo.js
+++ VUEJS/mapMutationwVuex/hijo.js
@@ -3,11 +3,12 @@ Vue.component('hijo',{
     `
     <div>
         <button @click="aumentar">+</button>
-        <button @click="disminuye">-</button>
+        <button @click="disminuye(2)">-</button>
         <h2>desde componente hijo: {{$store.state.numero}}</h2>
     </div>
     `,
     methods:{
+        // se utliza para methods, disminuy tiene parametro 
         ...Vuex.mapMutations(['aumentar','disminuye'])
     }
 })
