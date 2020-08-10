889932fe6aa2e47a9da84574c3c3e990d9a41aaa
abraham <fpatan@mail.ru>
2020-08-09 20:07:15 -0500

    final

--- Vue CLI/pro-2/src/App.vue
+++ Vue CLI/pro-2/src/App.vue
@@ -1,21 +1,28 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+  
+    <Cabecera></Cabecera>
+    <Lista></Lista>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Cabecera from './components/Cabecera.vue'
+import Lista from './components/Lista.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    Cabecera,
+    Lista
   }
 }
 </script>
 
+<!--
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -26,3 +33,4 @@ export default {
   margin-top: 60px;
 }
 </style>
+-->
---
+++ Vue CLI/pro-2/src/components/Lista.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <ul class="list-group">
+            <!--:key="item.id" => para indicarle que tiene elementos unicos -->
+  <li v-for="(item,index) of arrayOrdenado" :key="item.id" @click="aumentar(index)"
+  class="list-group-item d-flex justify-content-between align-items-center">
+    {{index}} - {{item.nombre}}
+    <span class="badge badge-primary badge-pill">{{item.cantidad}}</span>
+  </li>
+
+    <button class="btn btn-danger" @click="reiniciar">Borrar</button>
+</ul>
+
+
+    </div>    
+</template>
+
+
+<script>
+
+import {mapState, mapMutations} from 'vuex';
+
+
+export default {
+    name: 'Lista',
+
+    computed:{
+        ...mapState(['frutas']),
+
+        arrayOrdenado(state){
+            //dentro de sort tenemos un punto de comparacion 
+            return state.frutas.sort( (a,b) => b.cantidad - a.cantidad)
+        }
+    },
+    methods:{
+        ...mapMutations(['aumentar','reiniciar'])
+    }
+
+}
+</script>
+
+
