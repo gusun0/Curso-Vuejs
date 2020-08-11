ba193857bd94a53f65a39392b5b2c701300bee5e
abraham <fpatan@mail.ru>
2020-08-10 22:07:47 -0500

    cambios

---
+++ Vue CLI/boots/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    
+    <!-- es responsivo -->
+    <b-navbar toggleable="lg" type="dark" variant="danger">
+
+      <b-container>
+
+         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+        <b-collapse id="nav-collapse" is-nav>
+
+            <b-navbar-nav>
+              <b-nav-item to="/" active-class="active" exact>Home</b-nav-item>
+              <b-nav-item to="/about" active-class="active">About</b-nav-item>
+            </b-navbar-nav>
+
+        </b-collapse>
+
+
+      </b-container>
+
+
+    </b-navbar>
+    <router-view/>
+  </div>
+</template>
+
