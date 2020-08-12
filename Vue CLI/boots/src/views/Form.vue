7dcd494cfed8214df301905275999e2d38e94ad9
abraham <fpatan@mail.ru>
2020-08-11 19:54:20 -0500

    formularios

--- Vue CLI/boots/src/views/Form.vue
+++ Vue CLI/boots/src/views/Form.vue
@@ -5,13 +5,34 @@
         <form>  
             <!-- //filas -->
             <b-row> 
-                <b-col md="6">
+                <b-col md="3">
                     <!-- state es una propiedad -->
                     <b-form-input type="text" v-model="texto" :state="comprobar"></b-form-input>
                     <small>minimo 3 caracteres </small>
+                    <p>texto: {{texto}}</p>
                 </b-col>
+
+                <b-col md="3">
+                    <!-- siempre se va a relacionar con un v-model 
+                    select es la respuesta del usuario
+                     -->
+                    <b-form-select v-model="select" :options="animales"></b-form-select>
+                    <p>{{select}}</p>
+                    
+                </b-col>
+
+                <b-col md="3">
+                    <b-form-radio-group v-model="radio" :options="animalesRadio"></b-form-radio-group>
+
+                </b-col>
+
+
+                <b-col md="3">
+                    <b-form-checkbox-group v-model="cbox" :options="animalesRadio"></b-form-checkbox-group>
+                <p>{{cbox}}</p>
+                </b-col>
+
             </b-row>
-            <p>texto: {{texto}}</p>
         </form>
     
     </div>
@@ -23,7 +44,24 @@ export default {
     name: 'Forms',
     data(){
         return{
-            texto: ''
+            texto: '',
+            radio: null,
+            select: null,
+            cbox: [], 
+            animales: [
+                // estos valores viajan al select 
+                // el primer objeto dede de ser null 
+                {value:null, text: 'seleccione un animal'},
+                {value:'perro', text: 'Guau'},
+                {value:'gato', text: 'Miau'},
+                {value:'pato', text: 'Cuak'}
+            ],
+            animalesRadio: [
+                // {value:null, text: 'seleccione un animal'},
+                {value:'perro', text: 'Guau'},
+                {value:'gato', text: 'Miau'},
+                {value:'pato', text: 'Cuak'}
+            ]
         }
     },
     // hacemos una propiedad computada para manejar state de forma dinamica
