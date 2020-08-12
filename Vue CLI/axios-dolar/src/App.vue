d22d4aca8f5b565ad36d0fd1cb1aabb1ae9b9574
abraham <fpatan@mail.ru>
2020-08-11 23:29:52 -0500

    !

---
+++ Vue CLI/axios-dolar/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+ <!-- v-app para trabajar con vuetify -->
+  <v-app>
+    <!-- v-content ya no se usa, se usa v-main  -->
+    <v-main>
+<!-- para encerrar nuestas vistas => v-router  -->
+      <v-container>
+
+
+        <router-view/> 
+      </v-container>
+
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ Vue CLI/axios-dolar/src/views/Home.vue
@@ -0,0 +1,46 @@
+<template>
+<div>
+  <!-- v layout => row  -->
+  <!-- :wrap para que se respeten las 12 columnas  -->
+  <v-layout :wrap="true"> 
+    <!-- vflex => columna  -->
+    <v-flex xs12>
+        <v-card>
+           <v-date-picker 
+           v-model="picker"
+            full-width
+            locale="es-mx"   
+           >
+           </v-date-picker>
+        </v-card>
+        <v-card color="error" dark>
+          <!-- display 1 es para el tamaño de la letra -->
+          <v-card-text class="display-1 text-center"> 
+          650 - {{picker}}
+          </v-card-text>
+        </v-card>
+    </v-flex>
+
+
+
+  </v-layout>
+
+
+</div>
+
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  //cuando trabajamos con v-model tenemos que poner la data
+  data(){
+    return{
+      picker: ''
+    }
+  }
+  
+}
+</script>
