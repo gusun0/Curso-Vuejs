c5e2ebdcbb533c49b68855bd66e6ad586929777a
abraham <fpatan@mail.ru>
2020-08-12 20:25:42 -0500

    vuetify

---
+++ Vuetify/vuetify-1/src/views/Grid.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-container>
+        <v-row> 
+            <v-col cols="12" sm="6" xs="8">
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus ratione, quos cupiditate aliquam quaerat incidunt, eum numquam, odit consequatur officiis quis nihil voluptatum corrupti sunt non repellendus delectus. Aspernatur, ratione?</p>
+            </v-col>
+
+             <v-col cols=!2 sm="6" xs="4">
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus ratione, quos cupiditate aliquam quaerat incidunt, eum numquam, odit consequatur officiis quis nihil voluptatum corrupti sunt non repellendus delectus. Aspernatur, ratione?</p>
+            </v-col>
+
+        </v-row>
+    </v-container>
+</template>
