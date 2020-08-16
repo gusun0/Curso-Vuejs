ec10cd9e1b31890c27150fc02e53f435319e6713
abraham <fpatan@mail.ru>
2020-08-16 16:01:02 -0500

    card material design

--- Vuetify/vuetify-2/src/views/Home.vue
+++ Vuetify/vuetify-2/src/views/Home.vue
@@ -1,9 +1,51 @@
 <template>
   <v-container>
-    <v-layout>
-      <v-flex>
-      <h1>Hola mundo</h1>
+    <v-layout row wrap>
+      <v-flex xs12 sm6>
+
+      <v-card>
+        <!-- cuando tenemos una tarjeta esta dividiod en 3 secciones -->
+        <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"></v-img>
+
+        <v-card-title>
+          <h2>Lorem, ipsum dolor.</h2>
+        </v-card-title>
+
+        <v-card-text>
+          <p>Lorem ipsum dolor sit amet consectetur.</p>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-btn color="info" text>BOTON</v-btn>
+        </v-card-actions>
+        
+      </v-card>
       </v-flex>
+
+      <v-flex xs12 sm6>
+
+      <v-card>
+        <!-- cuando tenemos una tarjeta esta dividiod en 3 secciones -->
+        <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"></v-img>
+
+        <v-card-title>
+          <h2>Lorem, ipsum dolor.</h2>
+        </v-card-title>
+
+        <v-card-text>
+          <p>Lorem ipsum dolor sit amet consectetur.</p>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-btn color="info" text>BOTON</v-btn>
+        </v-card-actions>
+        
+      </v-card>
+
+      </v-flex>
+
+
+
     </v-layout>
   </v-container>
 </template>
