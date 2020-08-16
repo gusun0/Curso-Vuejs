7ace8ba3e7da80caa71ae6ae58e5c552aaf5a6e4
abraham <fpatan@mail.ru>
2020-08-15 23:14:19 -0500

    1

---
+++ Vuetify/vuetify-2/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-app>
+   
+    <!-- pide hacer un componente con esto   -->
+    <v-app-bar app class="warning">
+       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Hola</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn class="success">Ingreso</v-btn>
+      <v-btn class="error">Salir</v-btn>
+    </v-app-bar>
+
+    <v-navigation-drawer app v-model="drawer" temporary dark>
+      <v-layout mt-4 column align-center>
+        <v-flex>
+          <v-avatar>
+            <img src="https://randomuser.me/api/portraits/men/85.jpg" alt="">
+          </v-avatar>
+        </v-flex>
+        <v-flex>
+          <p class="white--text mt-3 headline">
+            Juan Perez
+
+          </p>
+          </v-flex>
+      </v-layout>
+    </v-navigation-drawer>
+
+    
+
+    
+    <!-- </v-app-bar> -->
+
+    <v-main>
+      <!-- <HelloWorld/> -->
+      <router-view>
+        
+      </router-view>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+   data() {
+    return{
+      drawer: true
+
+    }
+  }
+};
+</script>
---
+++ Vuetify/vuetify-2/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container>
+    <v-layout>
+      <v-flex>
+      <h1>Hola mundo</h1>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
