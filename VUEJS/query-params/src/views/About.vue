5b8592d98d623db811e2435331c4226de4667791
abraham <fpatan@mail.ru>
2020-08-17 21:09:38 -0500

    router link

---
+++ VUEJS/query-params/src/views/About.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <router-link :to="{query: {seccion: 1}}" class="btn btn-primary btn-block">1</router-link>
+    <router-link :to="{query: {seccion: 2}}" class="btn btn-primary btn-block">2</router-link>
+    <!-- <router-link :to="{query: {seccion: 3}}" class="btn btn-primary btn-block">3</router-link> -->
+  </div>
+</template>
+
+
+<script>
+export default {
+  
+  watch:{
+    // este pendiente de la query que se llama seccion 
+    "$route.query.seccion":{
+      inmediate: true,
+      handler(seccion){ // se puede poner solo un parametro, que seria newVal (newVal, oldVal)
+        console.log(`seccion ha cambiado: ${seccion}`);
+      } 
+    }
+  }
+}
+</script>
