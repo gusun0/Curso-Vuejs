db29263abc999bcd04e7487a36aa0467590b7931
abraham <fpatan@mail.ru>
2020-08-18 01:31:03 -0500

    vuepress

---
+++ vuepress/proyecto1/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  title: 'Hello VuePress',
+  description: 'Just playing around',
+
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guia', link: '/guia/' },
+      { text: 'External', link: 'https://google.com' }
+    ]
+  }
+
+}
---
+++ vuepress/proyecto1/guia/README.md
@@ -0,0 +1,1 @@
+# esta es la guia de gusun0
